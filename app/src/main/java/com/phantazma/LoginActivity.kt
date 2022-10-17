83fc9b3182ea0210fefcdc7c38f70fb9e9dfe69c
Lorena Pop <noemi97lorena@gmail.com>
2022-10-17 23:01:42 +0300

    new

---
+++ app/src/main/java/com/phantazma/LoginActivity.kt
@@ -0,0 +1,11 @@
+package com.phantazma
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class LoginActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_signin)
+    }
+}
---
+++ app/src/main/java/com/phantazma/SplashActivity.kt
@@ -0,0 +1,22 @@
+package com.phantazma
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+
+class SplashActivity : AppCompatActivity() {
+    private val SPLASH_TIME_OUT:Long = 1000
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+
+        Handler().postDelayed({
+            //Method will be executed once the timer is over
+            //Start app's main activity
+            startActivity(Intent(this, MainActivity::class.java))
+            finish()
+            overridePendingTransition(android.R.anim.fade_in, android.R.anim.fade_out)
+        }, SPLASH_TIME_OUT)
+    }
+}
